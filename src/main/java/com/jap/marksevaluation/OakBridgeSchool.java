d9179b8751116f6924f8925bce9d6de9861b664c
M V K <krishna.mugala9@gmail.com>
2022-08-18 22:41:44 +0530

    added source code and added java docs

--- src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
+++ src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
@@ -1,50 +1,146 @@
 package com.jap.marksevaluation;
+
 public class OakBridgeSchool {
-    public int[] calculateTotalMarks(int [] math,int science[],int[] english)
-    {
-       return new int[-2];
-    }
-
-    public int[] calculateTotalAverageMarksForEachStudent(int [] totalMarks, int noOfSubjects){
-        return new int[-1];
-    }
-    public int calculateAverageScienceMarks (int[] science)
-    {
-       return 0;
-    }
-    public int calculateAverageEnglishMarks (int[] english)
-    {
-       return 0;
-    }
-    public int calculateAverageMathMarks (int[] math) {
-      return 0;
-    }
-
     public static void main(String[] args) {
-        //Object of OakBridgeSchool class is created
+        int[] math = {88, 89, 100, 70, 60, 80, 35, 3, 25, 56};
+        int[] science = {80, 83, 99, 67, 56, 84, 38, 9, 32, 65};
+        int[] english = {90, 98, 100, 65, 54, 82, 40, 13, 45, 67};
+        String[] studentName = {"Michelle", "Kate", "Ann", "Tina", "Tom", "Sam", "Ria", "Pam", "Leena", "Leo",};
+        int[] rollNos = {102, 109, 101, 103, 104, 108, 110, 105, 106, 107};
         OakBridgeSchool oakBridgeSchool = new OakBridgeSchool();
-        // Initialize the arrays with 10 values of marks for each subject
-        int [] math = {};
-        int [] science = {};
-        int [] english = {};
-        // The total number of subjects
-        int noOfSubjects = 3;
-        // Initialize names of 10 Students
-        String[] studentNames = {};
-        // Initialize roll numbers of 10 Students
-        int [] rollNos = {};
-        // Call the methods to calculate total marks and average marks
-        int [] totalMarks = oakBridgeSchool.calculateTotalMarks(math,science,english);
-        int [] averageMarks = oakBridgeSchool.calculateTotalAverageMarksForEachStudent(totalMarks,noOfSubjects);
-        // Display the marks and average marks of each student
+        int[] result = oakBridgeSchool.calculateTotalMarks(math, science, english);
+        System.out.print("The total marks : ");
+        for (int j : result) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.print("The average marks of each student : ");
 
-        // Call the methods to calculate average marks of science, math and social subjects of the class
-        int averageScienceMarks = oakBridgeSchool.calculateAverageScienceMarks(science);
-        int averageMathMarks = oakBridgeSchool.calculateAverageMathMarks(math);
-        int averageEnglishMarks = oakBridgeSchool.calculateAverageEnglishMarks(english);
+        int[] averageMarksOfEachStudent = oakBridgeSchool.calculateTotalAverageMarksForEachStudent(result, 3);
+        for (int j : averageMarksOfEachStudent) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.print("The average science mark of each student : ");
+        int averageScienceMark = oakBridgeSchool.calculateAverageScienceMarks(science);
 
-        // Display the average marks scored by the class in subjects
+        System.out.print(averageScienceMark);
+// calcualte the average english marks
+        System.out.println();
+        System.out.print("The average English mark of each student : ");
+        int averageEnglishMark = oakBridgeSchool.calculateAverageEnglishMarks(english);
 
+        System.out.print(averageEnglishMark);
+// calculate the average math marks
+        System.out.println();
+        System.out.print("The average math mark of each student : ");
+        int averageMathMark = oakBridgeSchool.calculateAverageMathMarks(math);
+        System.out.print(averageMathMark);
+    }
 
+    /**
+     * It takes in three arrays of integers, and returns an array of integers
+     *
+     * @param math    an array of integers representing the marks in Maths
+     * @param science [23, 35, 14, 56, 78, 98, 12, 34, 56, 78]
+     * @param english an array of marks for English
+     * @return The total marks of the students in the three subjects.
+     */
+    public int[] calculateTotalMarks(int[] math, int[] science, int[] english) {
+        int[] total = new int[10];
+        if (math.length == 0 || science.length == 0 || english.length == 0) {
+            return null;
+        } else {
+            for (int i = 0; i < total.length; i++) {
+                total[i] = math[i] + science[i] + english[i];
+            }
+            return total;
+        }
+    }
+
+    /**
+     * It takes an array of total marks for each student and the number of subjects and returns an array of average marks
+     * for each student
+     *
+     * @param totalMarks   An array of integers containing the total marks of each student.
+     * @param noOfSubjects The number of subjects for which the marks are given.
+     * @return The average marks of each student.
+     */
+    public int[] calculateTotalAverageMarksForEachStudent(int[] totalMarks, int noOfSubjects) {
+        int[] e = new int[totalMarks.length];
+        System.arraycopy(totalMarks, 0, e, 0, totalMarks.length);
+
+        int[] avg = new int[10];
+        if (totalMarks.length == 0 || noOfSubjects == 0) {
+            return null;
+        } else {
+            for (int i = 0; i < 10; i++) {
+                avg[i] = e[i] / noOfSubjects;
+            }
+            return avg;
+        }
+    }
+
+    /**
+     * It takes an array of integers as input and returns the average of the integers in the array
+     *
+     * @param science an array of integers
+     * @return The average of the science marks.
+     */
+    public int calculateAverageScienceMarks(int[] science) {
+        int sum = 0;
+        int totalSum = 0;
+        int f = science.length;
+        if (f == 0) {
+            return -1;
+        } else {
+            for (int j : science) {
+                sum = sum + j;
+            }
+            totalSum += sum / 10;
+            return totalSum;
+        }
+    }
+
+    /**
+     * It takes an array of integers as input and returns the average of the integers in the array
+     *
+     * @param english an array of integers that represent the marks of the students in English.
+     * @return The average of the English marks.
+     */
+    public int calculateAverageEnglishMarks(int[] english) {
+        int sum = 0;
+        int totalSum = 0;
+        int f = english.length;
+        if (f == 0) {
+            return -1;
+        } else {
+            for (int j : english) {
+                sum = sum + j;
+            }
+            totalSum += sum / 10;
+            return totalSum;
+        }
+    }
+
+    /**
+     * It takes an array of integers as an argument and returns the average of the integers in the array
+     *
+     * @param math an array of integers
+     * @return The average of the math marks.
+     */
+    public int calculateAverageMathMarks(int[] math) {
+        int sum = 0;
+        int totalSum = 0;
+        int f = math.length;
+        if (f == 0) {
+            return -1;
+        } else {
+            for (int j : math) {
+                sum = sum + j;
+            }
+            totalSum += sum / 10;
+            return totalSum;
+        }
     }
 }
--- src/test/java/com/jap/marksevaluation/OakBridgeSchoolTest.java
+++ src/test/java/com/jap/marksevaluation/OakBridgeSchoolTest.java
@@ -1,6 +1,5 @@
 package com.jap.marksevaluation;
 
-import com.jap.marksevaluation.OakBridgeSchool;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,28 +7,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class OakBridgeSchoolTest
-{
-    int [] math;
-    int [] science;
-    int [] english;
+public class OakBridgeSchoolTest {
+    int[] math;
+    int[] science;
+    int[] english;
     int noOfSubjects;
     String[] studentNames;
-    int [] rollNos;
+    int[] rollNos;
     OakBridgeSchool oakBridgeSchool;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         oakBridgeSchool = new OakBridgeSchool();
-        math  = new int[]{88, 89, 100, 70, 60, 80, 35, 3, 25, 56};
+        math = new int[]{88, 89, 100, 70, 60, 80, 35, 3, 25, 56};
         science = new int[]{80, 83, 99, 67, 56, 84, 38, 9, 32, 65};
         english = new int[]{90, 98, 100, 65, 54, 82, 40, 13, 45, 67};
         noOfSubjects = 3;
         studentNames = new String[]{"Michelle", "Kate", "Ann", "Tina", "Tom", "Sam", "Ria", "Pam", "Leena", "Leo"};
         rollNos = new int[]{102, 109, 101, 103, 104, 108, 110, 105, 106, 107};
     }
+
     @After
-    public void tearDown(){
-        math  = null;
+    public void tearDown() {
+        math = null;
         science = null;
         english = null;
         noOfSubjects = 0;
@@ -39,62 +39,69 @@ public class OakBridgeSchoolTest
     }
 
     @Test
-    public  void givenMarksCalculateTotalAndReturnAnArraySuccess(){
+    public void givenMarksCalculateTotalAndReturnAnArraySuccess() {
         int[] expectedTotal = {258, 270, 299, 202, 170, 246, 113, 25, 102, 188};
-        assertArrayEquals(expectedTotal,oakBridgeSchool.calculateTotalMarks(math,science,english));
+        assertArrayEquals(expectedTotal, oakBridgeSchool.calculateTotalMarks(math, science, english));
 
-    }
-    @Test
-    public  void givenMarksCalculateTotalAndReturnAnArrayFailure(){
-        assertNull(oakBridgeSchool.calculateTotalMarks(new int[0],science,english));
-        assertNull(oakBridgeSchool.calculateTotalMarks(math,science,new int[0]));
-        assertNull(oakBridgeSchool.calculateTotalMarks(math,new int[0],english));
-        assertNull(oakBridgeSchool.calculateTotalMarks(new int[0],new int[0],new int[0]));
     }
 
     @Test
-    public  void givenTotalMarksArrayAndNumberOfSubjectsCalculateAverageReturnAnArraySuccess(){
+    public void givenMarksCalculateTotalAndReturnAnArrayFailure() {
+        assertNull(oakBridgeSchool.calculateTotalMarks(new int[0], science, english));
+        assertNull(oakBridgeSchool.calculateTotalMarks(math, science, new int[0]));
+        assertNull(oakBridgeSchool.calculateTotalMarks(math, new int[0], english));
+        assertNull(oakBridgeSchool.calculateTotalMarks(new int[0], new int[0], new int[0]));
+    }
+
+    @Test
+    public void givenTotalMarksArrayAndNumberOfSubjectsCalculateAverageReturnAnArraySuccess() {
         int[] totalArray = {258, 270, 299, 202, 170, 246, 113, 25, 102, 188};
         int[] expectedAverage = {86, 90, 99, 67, 56, 82, 37, 8, 34, 62};
-        assertArrayEquals(expectedAverage,oakBridgeSchool.calculateTotalAverageMarksForEachStudent(totalArray,noOfSubjects));
+        assertArrayEquals(expectedAverage, oakBridgeSchool.calculateTotalAverageMarksForEachStudent(totalArray, noOfSubjects));
     }
 
     @Test
-    public  void givenTotalMarksArrayAndNumberOfSubjectsCalculateAverageReturnAnArrayFailure(){
+    public void givenTotalMarksArrayAndNumberOfSubjectsCalculateAverageReturnAnArrayFailure() {
         int[] totalArray = {258, 270, 299, 202, 170, 246, 113, 25, 102, 188};
-        assertNull(oakBridgeSchool.calculateTotalAverageMarksForEachStudent(totalArray,0));
-        assertNull(oakBridgeSchool.calculateTotalAverageMarksForEachStudent(new int[0],3));
-        assertNull(oakBridgeSchool.calculateTotalAverageMarksForEachStudent(new int[0],0));
+        assertNull(oakBridgeSchool.calculateTotalAverageMarksForEachStudent(totalArray, 0));
+        assertNull(oakBridgeSchool.calculateTotalAverageMarksForEachStudent(new int[0], 3));
+        assertNull(oakBridgeSchool.calculateTotalAverageMarksForEachStudent(new int[0], 0));
     }
+
     @Test
-    public  void givenMathMarksArrayOfClassReturnAverageSuccess(){
+    public void givenMathMarksArrayOfClassReturnAverageSuccess() {
         int expectedMathAverage = 60;
-        assertEquals(expectedMathAverage,oakBridgeSchool.calculateAverageMathMarks(math));
+        assertEquals(expectedMathAverage, oakBridgeSchool.calculateAverageMathMarks(math));
     }
+
     @Test
-    public  void givenMathMarksArrayOfClassReturnAverageFailure(){
+    public void givenMathMarksArrayOfClassReturnAverageFailure() {
         int expectedMathAverage = -1;
-        assertEquals(expectedMathAverage,oakBridgeSchool.calculateAverageMathMarks(new int[0]));
+        assertEquals(expectedMathAverage, oakBridgeSchool.calculateAverageMathMarks(new int[0]));
     }
+
     @Test
-    public  void givenScienceMarksArrayOfClassReturnAverageSuccess(){
+    public void givenScienceMarksArrayOfClassReturnAverageSuccess() {
         int expectedScienceAverage = 61;
-        assertEquals(expectedScienceAverage,oakBridgeSchool.calculateAverageScienceMarks(science));
+        assertEquals(expectedScienceAverage, oakBridgeSchool.calculateAverageScienceMarks(science));
     }
+
     @Test
-    public  void givenScienceMarksArrayOfClassReturnAverageFailure(){
+    public void givenScienceMarksArrayOfClassReturnAverageFailure() {
         int expectedScienceAverage = -1;
-        assertEquals(expectedScienceAverage,oakBridgeSchool.calculateAverageScienceMarks(new int[0]));
+        assertEquals(expectedScienceAverage, oakBridgeSchool.calculateAverageScienceMarks(new int[0]));
     }
+
     @Test
-    public  void givenEnglishMarksArrayOfClassReturnAverageSuccess(){
+    public void givenEnglishMarksArrayOfClassReturnAverageSuccess() {
         int expectedEnglishAverage = 65;
-        assertEquals(expectedEnglishAverage,oakBridgeSchool.calculateAverageEnglishMarks(english));
+        assertEquals(expectedEnglishAverage, oakBridgeSchool.calculateAverageEnglishMarks(english));
     }
+
     @Test
-    public  void givenEnglishMarksArrayOfClassReturnAverageFailure(){
+    public void givenEnglishMarksArrayOfClassReturnAverageFailure() {
         int expectedEnglishAverage = -1;
-        assertEquals(expectedEnglishAverage,oakBridgeSchool.calculateAverageEnglishMarks(new int[0]));
+        assertEquals(expectedEnglishAverage, oakBridgeSchool.calculateAverageEnglishMarks(new int[0]));
     }
 
 }
